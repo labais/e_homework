2bcc943c25668126ee1186a7dac092360afb61d2
Mārtiņš Latkovskis <martins.latkovskis@gmail.com>
2021-04-11 14:45:14 +0300

    hole dissolving

--- Assets/Scripts/HoleAnimator.cs
+++ Assets/Scripts/HoleAnimator.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 
 public class HoleAnimator : MonoBehaviour
 {
-   
-
     void OnEnable()
     {
         Signals.Get<HoleGeneratedSignal>().AddListener(OnHoleGenerated);
@@ -20,21 +18,17 @@ public class HoleAnimator : MonoBehaviour
 
     private void OnHoleGenerated(Transform hole, Transform holeWall)
     {
-        Debug.Log("I know, right ?");
-
         holeWall.localScale = new Vector3(1, 50, 1);
         var beamDuration = .4f;
         var shakeMagnitude = .1f;
 
         // Less shaking for holes farther from camera
         var distance = Vector3.Distance(Camera.main.transform.position, holeWall.position);
-        var minD = 10;
-        var maxD = 20;
-        var percentageDistance = Mathf.Clamp01((distance - minD)/ (maxD - minD));
+        const float minD = 10;
+        const float maxD = 20;
+        var percentageDistance = Mathf.Clamp01((distance - minD) / (maxD - minD));
         var distanceFromCameraPenalty = (1 - percentageDistance);
-        Debug.Log($"d={distance} %={percentageDistance}");
-    
-    
+
         Sequence sequence = DOTween.Sequence();
         sequence.AppendCallback(() =>
         {
@@ -42,7 +36,22 @@ public class HoleAnimator : MonoBehaviour
             Signals.Get<ShakeCameraSignal>().Dispatch(beamDuration, shakeMagnitude * distanceFromCameraPenalty);
         });
         sequence.AppendInterval(beamDuration);
-        sequence.AppendCallback(() => { holeWall.gameObject.SetActive(false); });
-        sequence.AppendCallback(() => { hole.gameObject.SetActive(true); });
+        sequence.AppendCallback(() =>
+        {
+            holeWall.gameObject.SetActive(false);
+            hole.gameObject.SetActive(true);
+        });
+        sequence.AppendCallback(() =>
+        {
+            // StartCoroutine(Dissolve(hole));
+
+            //DOVirtual.EasedValue(0, 1, t, Ease.InOutCirc);
+            var mat = hole.GetComponent<Renderer>().material;
+            DOVirtual.Float(.05f, 1, 2.5f, (percentage) =>
+            {
+                var eased = DOVirtual.EasedValue(0, 1, percentage, Ease.OutQuad);
+                mat.SetFloat("_SliceAmount", eased);
+            });
+        });
     }
 }
